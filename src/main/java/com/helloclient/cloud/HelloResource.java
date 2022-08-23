b56a562a06d58b8cba0953af0ec25a5ada79f426
aparna <aparna.06cs03@gmail.com>
2022-08-23 22:35:33 +0530

    changes

--- src/main/java/com/helloclient/cloud/HelloResource.java
+++ src/main/java/com/helloclient/cloud/HelloResource.java
@@ -1,11 +1,9 @@
 package com.helloclient.cloud;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/rest/hello/client")
