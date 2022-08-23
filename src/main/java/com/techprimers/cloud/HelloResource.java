fc123edeb32747c99d97bc7b22591b0d13ee016b
aparna <aparna.06cs03@gmail.com>
2022-08-23 16:28:22 +0530

    changes

---
+++ src/main/java/com/techprimers/cloud/HelloResource.java
@@ -0,0 +1,33 @@
+package com.techprimers.cloud;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/rest/hello/client")
+public class HelloResource {
+
+
+	/*
+	 * @Autowired private RestTemplate restTemplate;
+	 */
+    
+    @Autowired
+    private HelloServerFeignClient feignclient;
+
+
+    @GetMapping
+    public String hello() {
+		
+		 return feignclient.getClient();
+    }
+
+    public String fallback(Throwable hystrixCommand) {
+        return "Fall Back Hello world";
+    }
+
+}
